fc72e2380f93824542ddeefdcd526ab0667113e6
destructodroid <omkaryel@gmail.com>
2023-01-21 12:44:28 +0530

    3 Screens and Buttons made
    3 Screens and Buttons made

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.SceneManager;
+using UnityEngine.SceneManagement;
 public class ButtonScript : MonoBehaviour
-
 
 {
     // Start is called before the first frame update
@@ -18,7 +17,19 @@ public class ButtonScript : MonoBehaviour
 
         
     }
-    void Restart()
+    public void NextScreen1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void NextScreen2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void NextScreen3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Restart()
     {
         SceneManager.LoadScene(0);
     }
